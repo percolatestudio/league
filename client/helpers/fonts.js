b10648dabe57f466e8b91b29ff33527431e57a2c
Tom Coleman <tom@thesnail.org>
2012-05-23 19:49:38 +1000

    Added webfont code

---
+++ client/helpers/fonts.js
@@ -0,0 +1,23 @@
+WebFontConfig = {
+  fonts: [ 
+    'Graduate', 'Sonsie One', 'Shojumaru', 'Trade Winds', 
+    'Holtwood One SC', 'Nova Square', 'Bangers', 'Carter One', 'Sansita One' 
+  ]
+}
+  
+// assumes all the fonts are latin..
+WebFontConfig.google = { 
+  families: _.map(WebFontConfig.fonts, function(f) { 
+    return f.replace(' ', '+') + '::latin'; 
+  })
+};
+
+(function() {
+  var wf = document.createElement('script');
+  wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+    '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+  wf.type = 'text/javascript';
+  wf.async = 'true';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(wf, s);
+}());
