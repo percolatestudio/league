f512a9db0835f978fe3bd7f21d0056bbbede4ae2
Dom Nguyen <d@supernormal.us>
2012-05-23 15:59:36 -0700

    added more fonts

--- client/helpers/fonts.js
+++ client/helpers/fonts.js
@@ -1,7 +1,7 @@
 WebFontConfig = {
   fonts: [ 
-    'Graduate', 'Sonsie One', 'Shojumaru', 'Trade Winds', 
-    'Holtwood One SC', 'Nova Square', 'Bangers', 'Carter One', 'Sansita One' 
+    'Graduate', 'Sonsie One', 'Shojumaru', 'Trade Winds', 'Open Sans'
+    'Holtwood One SC', 'Nova Square', 'Bangers', 'Carter One', 'Sansita One', 'Arvo', 'Pt+sans', 'Cabin' 
   ]
 }
   
