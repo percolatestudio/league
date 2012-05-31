d07a8b3a305068ce6b5f6598647ebfa188a16574
Dom Nguyen <d@supernormal.us>
2012-05-31 16:59:57 -0700

    commented out & deleted some JS to test whether FF memory leak is the culprit for poor performance

--- client/vendor/misc.js
+++ client/vendor/misc.js
@@ -3,7 +3,7 @@ $(function() {
   Kerning.js counterpart
   This runs anytime a new element is added to a page
   */
-  Kerning.live(); 
+/*   Kerning.live();  */
   $(".add_team.btn").live("click", function() {
     $(this).closest('li').toggleClass('current');
   });
